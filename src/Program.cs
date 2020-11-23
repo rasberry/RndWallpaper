5db9cb50ec73bc0b2cb2de3010f6aefdb2ae9ffc
rasberry <rasberry@hotmail.com>
2020-11-23 15:14:25 -0700

    saving progress

--- src/Program.cs
+++ src/Program.cs
@@ -19,133 +19,86 @@ namespace RndWallpaper
 			//
 
 			if (args.Length < 1) {
-				Console.WriteLine("must specify source folder for pictures");
+				Usage();
 				return;
 			}
-			string src = args[0];
-			if (!Directory.Exists(src)) {
-				Console.WriteLine($" folder \"{src}\" doesn't exist");
+			if (!ParseArgs(args)) {
 				return;
 			}
 
-			var rawList = Directory.GetFileSystemEntries(src);
-			var imgList = rawList.Where(file => {
-				var norm = file.ToLowerInvariant();
-				if (norm.EndsWith(".png")) { return true; }
-				if (norm.EndsWith(".jpg")) { return true; }
-				return false;
-			});
+			string file = null;
+			if (IsFolder) {
+				var rawList = Directory.GetFileSystemEntries(PicPath);
+				var imgList = rawList.Where(file => {
+					var norm = file.ToLowerInvariant();
+					if (norm.EndsWith(".png")) { return true; }
+					if (norm.EndsWith(".jpg")) { return true; }
+					return false;
+				});
 
-			//foreach(var img in imgList) {
-			//	Console.WriteLine(img);
-			//}
+				var rnd = RndSeed.HasValue ? new Random(RndSeed.Value) : new Random();
+				int index = rnd.Next(imgList.Count());
 
-			var rnd = new Random();
-			int index = rnd.Next(imgList.Count());
+				file = imgList.ElementAt(index);
+			}
+			else {
+				file = PicPath;
+			}
 
-			string file = imgList.ElementAt(index);
-			Console.WriteLine($"setting background to {file}");
+			Log.Message($"setting background to {file}");
 			Helpers.SetBackground(file);
 
-			var c = Helpers.GetAccentColor();
-			Console.WriteLine($"accent = {c}");
+			//var c = Helpers.GetAccentColor();
+			//Console.WriteLine($"accent = {c}");
 		}
+
+		static bool ParseArgs(string[] args)
+		{
+			var p = new Params(args);
+			if (p.Default("-s",out Style, PickWallpaperStyle.Fit).IsInvalid()) {
+				return false;
+			}
+			if (p.Default("-rs", out RndSeed).IsInvalid()) {
+				return false;
+			}
+
+			if (p.Expect(out PicPath,"image or folder path").IsBad()) {
+				return false;
+			}
+
+			if (Directory.Exists(PicPath)) {
+				IsFolder = true;
+			}
+			else if (!File.Exists(PicPath)) {
+				Log.CannotFindPath(PicPath);
+				return false;
+			}
+
+			return true;
+		}
+
+		static void Usage()
+		{
+			Log.Message(
+				   $"{nameof(RndWallpaper)} [options] (path of image or folder)"
+				+ "\nOptions:"
+				+ "\n -s (style)         Style of wallpaper (default 'Fit')"
+				+ "\n -rs (integer)      Random seed value (default system suplied)"
+				// + "\n -ta (boolean)      Enable or disable updating accent with the background (default leave as-is)"
+				+ "\n"
+				+ "\nAvailable Styles:"
+				+ "\n Center"
+				+ "\n Stretch"
+				+ "\n Fit"
+				+ "\n Fill"
+				+ "\n Span"
+			);
+		}
+
+		static PickWallpaperStyle Style = PickWallpaperStyle.None;
+		static int? RndSeed = null;
+		static string PicPath = null;
+		static bool IsFolder = false;
+
 	}
 }
-
-/*
-a029cc
-
-https://github.com/m417z/Windows-10-Color-Control/blob/master/WindowsThemeColorApi.cpp
-uxtheme.dll
-
- static HRESULT(WINAPI *GetUserColorPreference)(IMMERSIVE_COLOR_PREFERENCE *pcpPreference, bool fForceReload);
- static HRESULT(WINAPI *SetUserColorPreference)(const IMMERSIVE_COLOR_PREFERENCE *cpcpPreference, bool fForceCommit);
-
-struct IMMERSIVE_COLOR_PREFERENCE {
-	COLORREF color1;
-	COLORREF color2;
-};
-
-COLORREF 32 bit unsigned
-*/
-
-/*
-
-https://stackoverflow.com/questions/1061678/change-desktop-wallpaper-using-code-in-net
-
-
-public sealed class Wallpaper
-{
-	Wallpaper() { }
-
-	const int SPI_SETDESKWALLPAPER = 20;
-	const int SPIF_UPDATEINIFILE = 0x01;
-	const int SPIF_SENDWININICHANGE = 0x02;
-
-	[DllImport("user32.dll", CharSet = CharSet.Auto)]
-	static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-
-	public enum Style : int
-	{
-		Tiled,
-		Centered,
-		Stretched
-	}
-
-	public static void Set(Uri uri, Style style)
-	{
-		System.IO.Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
-
-		System.Drawing.Image img = System.Drawing.Image.FromStream(s);
-		string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
-		img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
-
-		RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-		if (style == Style.Stretched)
-		{
-			key.SetValue(@"WallpaperStyle", 2.ToString());
-			key.SetValue(@"TileWallpaper", 0.ToString());
-		}
-
-		if (style == Style.Centered)
-		{
-			key.SetValue(@"WallpaperStyle", 1.ToString());
-			key.SetValue(@"TileWallpaper", 0.ToString());
-		}
-
-		if (style == Style.Tiled)
-		{
-			key.SetValue(@"WallpaperStyle", 1.ToString());
-			key.SetValue(@"TileWallpaper", 1.ToString());
-		}
-
-		SystemParametersInfo(SPI_SETDESKWALLPAPER,
-			0,
-			tempPath,
-			SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
-	}
-}
-
-
-
-// https://docs.microsoft.com/en-us/uwp/api/Windows.System.UserProfile.UserProfilePersonalizationSettings?view=winrt-19041
-
-using Windows.System.UserProfile;
-
-// Pass in a relative path to a file inside the local appdata folder
-async Task<bool> SetWallpaperAsync(string localAppDataFileName)
-{
-	bool success = false;
-	if (UserProfilePersonalizationSettings.IsSupported())
-	{
-		var uri = new Uri("ms-appx:///Local/" + localAppDataFileName);
-		StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(uri);
-		UserProfilePersonalizationSettings profileSettings = UserProfilePersonalizationSettings.Current;
-		success = await profileSettings.TrySetLockScreenImageAsync(file);
-	}
-	return success;
-}
-
-
-*/
