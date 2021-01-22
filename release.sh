50d5cf1e174e6f10cf315a68d53ba302712f1a9b
rasberry <rasberry@hotmail.com>
2021-01-21 22:11:00 -0700

    added check for existing release zip file

--- release.sh
+++ release.sh
@@ -27,6 +27,9 @@ _publishone() {
 	if [ -z "$1" ]; then echo "_publishone invalid rid"; exit 1; fi
 	if [ -z "$2" ]; then echo "_publishone invalid version"; exit 1; fi
 
+	local zipFile="./$1-native-$2.7z"
+	if [[ -f "$zipFile" ]]; then echo "$zipFile already exists"; exit 2; fi
+
 	list="make.sh.tmp.txt"
 
 	# do a build / publish with RID
@@ -35,7 +38,7 @@ _publishone() {
 
 	# package native build
 	find "./$outNative/" -maxdepth 1 -type f > "$list"
-	7z a -mx=9 -ms=on -i@"$list" "./$1-native-$2.7z"
+	7z a -mx=9 -ms=on -i@"$list" "$zipFile"
 
 	rm "$list"
 }
@@ -44,7 +47,7 @@ publish() {
 	# https://docs.microsoft.com/en-us/dotnet/core/rid-catalog
 	rid="$(_getrid)"
 	#change version number here
-	ver="1.0.1"
+	ver="1.0.2"
 
 	if [ -n "$rid" ]; then
 		_publishone "$(_getrid)" "$ver" "netcoreapp3.1"
